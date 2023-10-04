f91a46604c48747b699c8dbd66773c16f4415f2c
Jane <116977119+critakara@users.noreply.github.com>
2023-10-03 23:34:26 -0400

    Added the ruby code

---
+++ full_name.rb
@@ -0,0 +1,19 @@
+# Write a program which asks for a person's first name, then middle, then last. It should store each of these parts in an array. Finally, it should greet the person using their full name. Call the program full_name.rb.
+
+fullName = []
+
+puts "What is your first name?"
+firstName = gets.chomp # get the first name
+fullName.push(firstName)
+
+puts "What is your middle name?"
+middleName = gets.chomp # get the middle name
+fullName.push(middleName)
+
+puts "What is your last name?"
+lastName = gets.chomp # get the last name
+fullName.push(lastName)
+
+for i in fullName
+  print i + " "
+end
