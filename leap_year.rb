f34915b60dcd504e83640229e5e2be97d21d0839
Jane <116977119+critakara@users.noreply.github.com>
2023-10-20 10:14:17 -0400

    should be viewable now

---
+++ leap_year.rb
@@ -0,0 +1,27 @@
+# Write a program, leap_year.rb. It will ask the user for a starting year and an ending year, and it will then print out all the leap years between them, including the starting or ending year if those are leap years. The rules for leap years are: A leap year is divisible by 4, except for years that are divisible by 100 -- those aren't leap years -- except for years that are divisible by 400, which ARE leap years.
+
+puts "Please enter starting year"
+yearStart = gets.chomp # get the year
+yearStart = yearStart.to_i # make it into an int
+
+puts "Please enter an ending year"
+yearEnd = gets.chomp
+yearEnd = yearEnd.to_i
+
+puts "" # add an extra newline at the end just to clean up the look
+
+# get each year between a and b
+# check if % 4 = 0 and % 100 != 0 then print if it is
+# check if % 400 = 0 then print
+# if not move on
+
+for i in yearStart..yearEnd
+  if i % 4 == 0 && i % 100 != 0 # all years divisible by 4 but not 100
+    puts i
+  elsif i % 400 == 0 # checks if it is divisible by 400 afterwards
+    puts i
+  else
+    # nothing here because the else case is to not do anythingx
+  end
+end
+
---
+++ sorted_words.rb
@@ -0,0 +1,28 @@
+# Write a program called sorted_words.rb. It should prompt the user for words and add each to an array. The user should be able to add as many words as they like, until they just hit enter to return a blank word. Then sort the array using the sort method and print it out.
+
+
+# while not null
+# get the word in
+# store in array
+# when it finishes
+# sort
+
+wordList = [] # intialize the word list as an array
+
+puts "Please enter a word"
+word = gets.chomp # get the word in
+
+while word != ""
+  wordList.push(word)
+  puts "Please enter a word or press enter to sort"
+  word = gets.chomp
+  
+end
+
+wordList.sort! # sort the words and changet the array so I don't have to make a new arraw
+
+for i in wordList
+  puts i
+end
+
+
